834d79c66e6b4a92f454ac94033cca1f52363a14
Yann Wilhelm <yannwilhelm@yahoo.de>
2020-12-04 00:48:34 +0100

    optimized day 3

--- src/Day03.jl
+++ src/Day03.jl
@@ -4,52 +4,46 @@ using AdventOfCode2020
 # TODO rewrite using OffsetArrays. Can I do something fun with OffsetArray
 # indexed with CartesianIndex?
 
-function solve(input::AbstractString = readinput(
-    joinpath(@__DIR__, "Day03_input.txt")))
-    data = splitlines(input)
-    (Part1=part1(data), Part2=part2(data))
+const input = readinput(joinpath(@__DIR__, "Day03_input.txt"))
+const start_pos = CartesianIndex(1, 1)
+const part1_slope = CartesianIndex(3, 1)
+const part2_slopes = [
+            CartesianIndex(1, 1), CartesianIndex(3, 1), 
+            CartesianIndex(5, 1), CartesianIndex(7, 1), 
+            CartesianIndex(1, 2)]
+
+function solve(i::AbstractString = input, start::CartesianIndex = start_pos,
+           slope = part1_slope, slopes = part2_slopes
+              )
+    terrain = splitlines(input) |> parsemap
+
+    solution1 = count_trees(terrain, start, slope)
+    solution2 = mapreduce(s -> count_trees(terrain, start, s), *, slopes)
+
+    return (Part1=solution1, Part2=solution2)
 end
 
 function parsemap(data)
     # this yields an array with its colums as the rows of input, which makes it
     # intuitively indexable, e.g [3,1] = "three right, one down"
     parserow(r) = [isequal('#', c) ? 1 : 0 for c in r]
-    return map = mapreduce(parserow, hcat, data)
+    return terrain = mapreduce(parserow, hcat, data)
 end
 
-function trajectory(start, slope, wrap, n)
+function trajectory(start, slope, wrap, N)
     #TODO only works with indizes <= 2. How can I generalize that?
-    path = accumulate(1:n; init=start) do x, _
+    path = accumulate(1:N; init=start) do x, _
         # mod1 prevents us from wrapping to 0
         CartesianIndex(mod1((x[1] + slope[1]), wrap), x[2]+slope[2])
     end
     return path
 end
 
-function count_trees(data::AbstractArray, start, slope)
-    map = parsemap(data)
-    wrap = size(map)[1]
-    num_steps = ceil((size(map)[2] / slope[2]) - 1)
+function count_trees(terrain::AbstractArray, start, slope)
+    wrap = size(terrain)[1]
+    num_steps = ceil((size(terrain)[2] / slope[2]) - 1)
     path = trajectory(start, slope, wrap, num_steps)
-    num_trees = sum([map[p] for p in path])
+    num_trees = sum([terrain[p] for p in path])
     return num_trees
-end
-
-function part1(data::AbstractArray,
-           start::CartesianIndex = CartesianIndex(1, 1), 
-           slope::CartesianIndex = CartesianIndex(3, 1))
-    count_trees(data, start, slope)
-end
-
-function part2(data::AbstractArray,
-           start::CartesianIndex = CartesianIndex(1, 1), 
-           slopes = [
-                    CartesianIndex(1, 1),
-                    CartesianIndex(3, 1),
-                    CartesianIndex(5, 1),
-                    CartesianIndex(7, 1),
-                    CartesianIndex(1, 2)])
-    # TODO wrap this, so that don't have to pass everything 
-    reduce(*, [count_trees(data, start, slope) for slope in slopes])
 end
 end # module
