4c5e3d8498cde9b4acae250676e8fd8760eee577
Yann Wilhelm <yannwilhelm@yahoo.de>
2020-12-04 11:24:35 +0100

    optimized day3 and the main module

--- src/AdventOfCode2020.jl
+++ src/AdventOfCode2020.jl
@@ -3,23 +3,23 @@ module AdventOfCode2020
 using StrLiterals
 using StrFormat
 
-for i in 1:26
-    include(joinpath(@__DIR__, f"Day\%02d(i).jl"))
-end
-
 export  Day01, Day02, Day03, Day04, Day05, Day06, Day07, Day08, Day09, 
         Day10, Day11, Day12, Day13, Day14, Day15, Day16, Day17, Day18, 
         Day19, Day20, Day21, Day22, Day23, Day24, Day25, Day26, 
         readinput, splitlines
 
-function readinput(path::AbstractString)
+function readinput(path)
     open(path, "r") do f
         read(f, String)
     end
 end
 
-function splitlines(input::AbstractString)
+function splitlines(input)
     split(input, "\n", keepempty=false)
+end
+
+for i in 1:26
+    include(joinpath(@__DIR__, f"Day\%02d(i).jl"))
 end
 
 function setup_project()
@@ -36,17 +36,18 @@ function setup_project()
                 write(io, f"""module Day\%02d(i)
 using AdventOfCode2020
 
-function solve(input::AbstractString = readinput(
-    joinpath(@__DIR__, "Day\%02d(i)_input.txt")))
-    data = split(input, "\n", keepempty=false)
+const input = readinput(joinpath(@__DIR__, "Day\%02d(i)_input.txt")) |> 
+             splitlines
+
+function solve(data = input)
     (Part1=part1(data), Part2=part2(data))
 end
 
-function part1(data::AbstractArray)
+function part1(data)
     missing
 end
 
-function part2(data::AbstractArray)
+function part2(data)
     missing
 end
 end # module""")
--- src/Day03.jl
+++ src/Day03.jl
@@ -4,30 +4,31 @@ using AdventOfCode2020
 # TODO rewrite using OffsetArrays. Can I do something fun with OffsetArray
 # indexed with CartesianIndex?
 
-const input = readinput(joinpath(@__DIR__, "Day03_input.txt"))
+function parsemap(data)
+    # Note to self: this yields an array with its colums as the rows of input,
+    # which makes it intuitively indexable, e.g [3,1] = "three right, one
+    # down". Think of it in this way and don't get confused by how it is
+    # represented in the REPL.
+    
+    parserow(r) = [isequal('#', c) ? 1 : 0 for c in r] 
+    return mapreduce(parserow, hcat, data) 
+end
+
+const input =   readinput(joinpath(@__DIR__, "Day03_input.txt")) |> 
+                splitlines |> parsemap
 const start_pos = CartesianIndex(1, 1)
 const part1_slope = CartesianIndex(3, 1)
-const part2_slopes = [
-            CartesianIndex(1, 1), CartesianIndex(3, 1), 
-            CartesianIndex(5, 1), CartesianIndex(7, 1), 
-            CartesianIndex(1, 2)]
+const part2_slopes = [CartesianIndex(1, 1), CartesianIndex(3, 1),
+                      CartesianIndex(5, 1), CartesianIndex(7, 1),
+                      CartesianIndex(1, 2)]
 
-function solve(i::AbstractString = input, start::CartesianIndex = start_pos,
-           slope = part1_slope, slopes = part2_slopes
-              )
-    terrain = splitlines(input) |> parsemap
+function solve(terrain = input, start = start_pos, slope = part1_slope, 
+               slopes = part2_slopes)
 
     solution1 = count_trees(terrain, start, slope)
     solution2 = mapreduce(s -> count_trees(terrain, start, s), *, slopes)
 
     return (Part1=solution1, Part2=solution2)
-end
-
-function parsemap(data)
-    # this yields an array with its colums as the rows of input, which makes it
-    # intuitively indexable, e.g [3,1] = "three right, one down"
-    parserow(r) = [isequal('#', c) ? 1 : 0 for c in r]
-    return terrain = mapreduce(parserow, hcat, data)
 end
 
 function trajectory(start, slope, wrap, N)
@@ -39,11 +40,11 @@ function trajectory(start, slope, wrap, N)
     return path
 end
 
-function count_trees(terrain::AbstractArray, start, slope)
+function count_trees(terrain, start, slope)
     wrap = size(terrain)[1]
     num_steps = ceil((size(terrain)[2] / slope[2]) - 1)
     path = trajectory(start, slope, wrap, num_steps)
-    num_trees = sum([terrain[p] for p in path])
+    num_trees = sum((terrain[p] for p in path))
     return num_trees
 end
 end # module
