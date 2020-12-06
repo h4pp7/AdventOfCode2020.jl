59280e4aeb77d311e3e995de6716271aa4f7689f
Yann Wilhelm <yannwilhelm@yahoo.de>
2020-12-06 08:04:50 +0100

    solved day 5 part 2, optimized part 1

--- src/Day05.jl
+++ src/Day05.jl
@@ -1,19 +1,37 @@
 module Day05
 using AdventOfCode2020
+using LinearAlgebra
 
 const raw_data = readinput(joinpath(@__DIR__, "Day05_input.txt"))
 
-const mappings = Dict('F' => '0', 'B' => '1', 'L' => '0', 'R' => '1', '\n' => ' ') 
+@inline function converttodec(line)
+        idvector = in.(collect(line), "BR")
+        # dot product to multiply each number in the vevor with 0, 2, 4, 8 etc
+        # to get the decimal number
+        dot(idvector, 2 .^(9:-1:0))
+end
 
 function solve(input = raw_data)
-    (Part1=part1(input), Part2=part2(data))
+    (Part1=part1(input), Part2=part2(input))
 end
 
-function part1(input = raw_data)
-    solution1 = maximum(parse.(Int, split(join((mappings[c] for c in input))), base = 2))
+function part1(data = raw_data)
+    maximum((converttodec(line) for line in splitlines(data)))
 end
 
-function part2(data)
-    missing
+function part2(data = raw_data)
+    lines = splitlines(data)  
+    (minid, maxid, xorid) = 1024, 0, 0
+    for line in lines
+        id = converttodec(line)
+        maxid = max(maxid, id)
+        minid = min(minid, id)
+        xorid ⊻= id # xor all ids with each other
+        # since xor all ids is 0, my missing one must be what's left
+    end
+    for i = minid:maxid
+        xorid ⊻= i
+    end
+    xorid
 end
 end # module
