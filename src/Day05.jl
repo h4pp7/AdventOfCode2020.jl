0bd05d199442cd03f18e9e42e0a5748414266e9b
Yann Wilhelm <yannwilhelm@yahoo.de>
2020-12-06 05:14:32 +0100

    Solved day 5 part 1

--- src/Day05.jl
+++ src/Day05.jl
@@ -3,17 +3,14 @@ using AdventOfCode2020
 
 const raw_data = readinput(joinpath(@__DIR__, "Day05_input.txt"))
 
-function get_id(seatnumber::String)
-    1 + 1
-end
+const mappings = Dict('F' => '0', 'B' => '1', 'L' => '0', 'R' => '1', '\n' => ' ') 
 
 function solve(input = raw_data)
-    data = split(input, "\n", keepempty=false)
-    (Part1=part1(data), Part2=part2(data))
+    (Part1=part1(input), Part2=part2(data))
 end
 
-function part1(data)
-    missing
+function part1(input = raw_data)
+    solution1 = maximum(parse.(Int, split(join((mappings[c] for c in input))), base = 2))
 end
 
 function part2(data)
