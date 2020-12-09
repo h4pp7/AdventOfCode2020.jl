67308a529bd8c47263dc8010fb44de6c6ceed496
Yann Wilhelm <yannwilhelm@yahoo.de>
2020-12-10 00:16:33 +0100

    solved day 8

--- src/Day08.jl
+++ src/Day08.jl
@@ -1,18 +1,73 @@
 module Day08
 using AdventOfCode2020
 
-function solve(input::AbstractString = readinput(
-    joinpath(@__DIR__, "Day08_input.txt")))
-    data = split(input, "
-", keepempty=false)
-    (Part1=part1(data), Part2=part2(data))
+const raw_data = readinput(joinpath(@__DIR__, "Day08_input.txt"))
+
+function acc(A, PC, operand)
+    A += operand
+    PC += 1
+    A, PC
 end
 
-function part1(data::AbstractArray)
-    missing
+function jmp(A, PC, operand)
+    PC += operand  
+    A, PC
 end
 
-function part2(data::AbstractArray)
-    missing
+function nop(A, PC, operand)
+    PC += 1
+    A, PC
+end
+
+const opcodes = Dict{String, Function}("acc" => acc, "jmp" => jmp, "nop" => nop)
+
+function run(A, PC, C, lines)
+    while PC ∉ C && PC <= length(lines)
+        push!(C, PC) 
+        l = lines[PC]
+        operand = parse(Int, l[5:end])
+        A, PC = opcodes[l[1:3]](A, PC, operand)
+    end
+    A, PC
+end
+
+function solve(data = raw_data)
+    A = 0
+    PC = 1
+    C = []
+    lines = splitlines(raw_data)
+    solution1, _ = run(A, PC, C, lines)
+    solution2 = find_bug(lines)
+    (Part1=solution1, Part2=solution2)
+end
+
+function find_bug(lines = splitlines(raw_data))
+    A = 0
+    PC = 1
+    C = []
+    lines = splitlines(raw_data)
+    run(A, PC, C, lines)
+
+    nj = [i for i in C if lines[i][1:3] == "nop" || lines[i][1:3] == "jmp"]
+
+    for i in nj
+        A = 0
+        PC = 1
+        C = []
+        if lines[i][1:3] == "jmp" 
+            lines[i] = replace(lines[i], "jmp" => "nop")
+        else 
+            lines[i] = replace(lines[i], "nop" => "jmp")
+        end
+        A, PC = run(A, PC, C, lines)
+        if PC == length(lines) + 1
+            return A
+        end
+        if lines[i][1:3] == "jmp" 
+            lines[i] = replace(lines[i], "jmp" => "nop")
+        else 
+            lines[i] = replace(lines[i], "nop" => "jmp")
+        end
+    end
 end
 end # module
