96e32165265b9013a49832f46f1998c358ce7adf
Yann Wilhelm <yannwilhelm@yahoo.de>
2020-12-10 21:18:32 +0100

    solved Day 9

--- src/Day09.jl
+++ src/Day09.jl
@@ -1,18 +1,45 @@
 module Day09
 using AdventOfCode2020
+using Combinatorics
 
-function solve(input::AbstractString = readinput(
-    joinpath(@__DIR__, "Day09_input.txt")))
-    data = split(input, "
-", keepempty=false)
-    (Part1=part1(data), Part2=part2(data))
+const raw_data = readinput(joinpath(@__DIR__, "Day09_input.txt"))
+
+function solve(data = raw_data)
+    N = [parse(Int, n.match) for n in eachmatch(r"\d+", data)]
+    solution1, hit_ind = part1(N, 25)
+    solution2 = part2(N, hit_ind)
+    (Part1=solution1, Part2=solution2)
 end
 
-function part1(data::AbstractArray)
-    missing
+function part1(N, w)
+    # TODO replace combinations with something that just operates on views --
+    # would that reduce the allocs? Part 2 makes 1 alloc with 16 byte :shrug:
+    # But maybe combinations is not the culprit...
+    @views for i in 1:length(N) - w
+        s = any(sum(c) == N[i+w] for c in combinations(N[i:i+w-1], 2))
+        if s == false
+            return N[i+w], i+w
+        end
+    end
 end
 
-function part2(data::AbstractArray)
-    missing
+function part2(N, hit_ind)
+    i = 1 
+    j = 1
+
+    @views while sum(N[hit_ind-j:hit_ind-i]) != N[hit_ind]
+        s = sum(N[hit_ind-j:hit_ind-i])
+        if s == N[hit_ind]
+            break
+        elseif s > N[hit_ind]
+            i += 1
+        else
+        j += 1
+        end
+    end
+    max = maximum(@view N[hit_ind-j:hit_ind-i])
+    min = minimum(@view N[hit_ind-j:hit_ind-i])
+    return max + min
 end
+
 end # module
--- test/tests.jl
+++ test/tests.jl
@@ -1,4 +1,4 @@
-module Test
+module AoCtest
 using AdventOfCode2020
 using Test
 
@@ -18,33 +18,6 @@ function day05()
 
         @test AdventOfCode2020.Day05.part1(testdata1) == 820
         @test AdventOfCode2020.Day05.part2(testdata2) == 5
-    end
-end
-
-function day07()
-    @testset "Day 7" begin
-        testdata = "light red bags contain 1 bright white bag, 2 muted yellow bags.\n" *
-        "dark orange bags contain 3 bright white bags, 4 muted yellow bags.\n" *
-        "bright white bags contain 1 shiny gold bag.\n" *
-        "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.\n" *
-        "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.\n" *
-        "dark olive bags contain 3 faded blue bags, 4 dotted black bags.\n" *
-        "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.\n" *
-        "faded blue bags contain no other bags.\n" *
-        "dotted black bags contain no other bags.\n"
-
-        testdata2 = "shiny gold bags contain 2 dark red bags.\n" *
-                    "dark red bags contain 2 dark orange bags.\n" *
-                    "dark orange bags contain 2 dark yellow bags.\n" *
-                    "dark yellow bags contain 2 dark green bags.\n" *
-                    "dark green bags contain 2 dark blue bags.\n" *
-                    "dark blue bags contain 2 dark violet bags.\n" *
-                    "dark violet bags contain no other bags.\n"
-
-        testrule = "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags."
-        @test AdventOfCode2020.Day07.parse_rules(testrule) == Dict("muted yellow" => ["shiny gold", "faded blue"])
-        @test AdventOfCode2020.Day07.part1("shiny gold", testdata) == 4
-        @test AdventOfCode2020.Day07.findbagcontents("shiny gold", AdventOfCode2020.splitlines(testdata)) == (["dark olive", "vibrant plum"], 3)
     end
 end
 end # module
