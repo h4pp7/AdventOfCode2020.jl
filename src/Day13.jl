9dd7a473fd1325afaf046ccec873415e8b1e997a
Yann Wilhelm <yannwilhelm@yahoo.de>
2020-12-13 20:36:38 +0100

    solved day 13

--- src/Day13.jl
+++ src/Day13.jl
@@ -1,18 +1,52 @@
 module Day13
 using AdventOfCode2020
 
-const input = readinput(joinpath(@__DIR__, "Day13_input.txt")) |> 
-             splitlines
+const raw_data = readinput(joinpath(@__DIR__, "Day13_input.txt"))
 
-function solve(data = input)
-    (Part1=part1(data), Part2=part2(data))
+function parse_data(data = raw_data)
+    dep, lines = split(data) 
+    dep = parse(Int, dep)
+    lines = replace(lines, "x" => "0") |> x -> split(x, ",") |> 
+            x -> parse.(Int, x)
+    dep, lines
 end
 
-function part1(data)
-    missing
+function solve(data = raw_data)
+    dep, lines = parse_data(data)
+    (Part1=part1(dep, lines), Part2=part2(lines))
 end
 
-function part2(data)
-    missing
+function part1(dep, lines)
+    search = true
+    id = 0
+    t = dep - 1
+    while search
+        t += 1
+        for l in lines 
+            l == 0 && continue
+            n = t % l
+            if n == 0
+                id = l
+                search = false
+                break
+            end
+        end
+    end
+    (t - dep) * id  
+end
+
+function part2(lines)
+    # TODO: implement using Chinese Remainder Theorem
+    t = 0
+    step = lines[1]
+    for (i, l) in enumerate(lines)
+        i == 1 && continue
+        l == 0 && continue
+        while (t + i - 1) % lines[i] != 0
+            t += step 
+        end
+        step *= lines[i]
+    end
+    t
 end
 end # module
