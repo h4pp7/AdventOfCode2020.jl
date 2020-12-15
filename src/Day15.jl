f3c78668df4e32458afcf2b3d2a5c4f529f43ee1
Yann Wilhelm <yannwilhelm@yahoo.de>
2020-12-15 16:18:34 +0100

    solved day 15

--- src/Day15.jl
+++ src/Day15.jl
@@ -1,18 +1,26 @@
 module Day15
 using AdventOfCode2020
 
-const input = readinput(joinpath(@__DIR__, "Day15_input.txt")) |> 
-             splitlines
+const raw_input = "2,15,0,9,1,20"
 
-function solve(data = input)
-    (Part1=part1(data), Part2=part2(data))
+function solve(input = raw_input)
+    (Part1=play(input, 2020), Part2=play(input, 30000000))
 end
 
-function part1(data)
-    missing
-end
+function play(input, N)
+    list = parse.(Int, split(input, ","))
+    d = Dict(list[i] => i for i in 1:length(list) - 1)
+    last_n = last(list)
 
-function part2(data)
-    missing
+    for i in length(d)+2:N
+        if last_n in keys(d) 
+            current = i - 1 - d[last_n]
+        else
+            current = 0
+        end
+        d[last_n] = i - 1
+        last_n = current
+    end
+    return last_n
 end
 end # module
