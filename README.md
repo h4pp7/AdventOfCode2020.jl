d826be829e68df63ebc41738a8ea0c9657f4b28f
Yann Wilhelm <yannwilhelm@yahoo.de>
2020-12-24 17:27:38 +0100

    optimized day 24 alot by removing a bunch of unnecesary loops

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Overview
 Julia Package with my attempt at [Advent of Code 2020](https://adventofcode.com/2020/).
-Just for fun and for learning Julia.
+Just for fun and for learning Julia (and programming in general).
 
 As a soft rule I'm trying to only use Base packages, since I want to learn the
 language.
@@ -10,7 +10,8 @@ language.
 The commits with the first submitted correct solutions for each day are tagged
 with git tags. `v0.1` for day 1, `v0.2` for day 2 etc.
 
-## Day 17
+## Notes (to self)
+### Day 17
 I could reuse the core of Day 10, but simplified, because there are no cells,
 who aren't alive or dead. Since I solved part 1 to work with arbitrary
 dimensions, the second part only needed small changes. I'm growing the pocket
@@ -20,14 +21,14 @@ found a way to do that and retain combatibility with arbitrary dimensions,
 though. Maybe I can use [this idea](https://julialang.org/blog/2016/02/iteration/) for summing along a single
 dimension...
 
-## Day 18
+### Day 18
 Implemented half a [Shunting-yard
 algorithm](https://en.wikipedia.org/wiki/Shunting-yard_algorithm). It converts
 a string in infix notation to a string in reverse polish notation, but it can
 only deal with '*' and '+'. I then evaluate that string by splitting it up and
 mapping over it with Meta.parse and eval.
 
-## Day 19
+### Day 19
 **unfinished**
 TODO: complete implementing [CYK algorithm](https://en.wikipedia.org/wiki/CYK_algorithm). Eliminating unit rules is missing.
 
@@ -35,20 +36,20 @@ Solved part 2 by building a regex pattern. Just iterated over the rules and
 replaced numbers by the rule text of the referenced rule until no numbers where
 left.
 
-## Day 20
+### Day 20
 **unfinished**
-TODO: Actually build the map. The monster search function should work like.
+TODO: Actually build the map. The monster search function should work.
 
 Solved part 1 bei not building the map and just storing the border tiles. Just brute force search for the corner tiles.
 
-## Day 21
+### Day 21
 Make a dictionary from allergens to ingredients, with only the intersection of
 all the ingredients listet for that allergen. Count all the ingredients not in
 that dictionary for part 1. For part 2 I just reused Day 16. Go through the
 dictionary, find the allergen that has only one ingredient listet, pop that
 allergen out and into a confirmed list. Repeat until the dictionary is empty.
 
-## Day 22
+### Day 22
 For part 1 just implemented the instructions step by step, basically. For part
 2 call the function recursively in the while loop, when the condition for the
 recursive game is met. Store the previous game turns to check for
@@ -56,7 +57,7 @@ non-terminating games. I tried pretty long to do it with a recursive call at
 the end of the function instead of a while loop. But never could get the right
 solution and had some weird interactions between the decks and their copies.
 
-## Day 23
+### Day 23
 Naively implemented part 1. Instead of wrapping around I used `circularshift`
 to rotate the contents of the vector, so that the current cup was always the
 first element.
@@ -67,3 +68,15 @@ find out for quite some time why it still ran for a minute without terminating
 (before I stopped it). For some reason I put 1_000_000_000 as the size of the
 vector and 10_000_000_000 as the number of steps. So 10^9 and 10^10 instead of
 10^6 and 10^7 🤦
+
+### Day 24
+Made a struct as a wrapper around a dictionary from cube coordinates to
+true/false for color. That way I have (0,0,0) as the origin coordinates and I
+can parse and reduce the paths to relative coordinates. [This website](https://www.redblobgames.com/grids/hexagons) 
+about hexagonal grids is amazing. The getfunction for the struct returns
+`false` as default, that way I don't have to bother about extending. The struct
+is not really needed, I could use the dict directly, but it makes it a bit more
+convenient and I wanted to learn more about custom Types. I initially wanted to
+make a real custom AbstractArray with negative indexing etc (c.f. 
+[Arrays with custom indices](https://docs.julialang.org/en/v1/devdocs/offset-arrays/#man-custom-indices))
+but kept it simple in the end.
--- src/Day24.jl
+++ src/Day24.jl
@@ -4,34 +4,46 @@ using AdventOfCode2020
 const raw_input = readinput(joinpath(@__DIR__, "Day24_input.txt"))
 
 struct Hexgrid
-    data::Dict{CartesianIndex, Bool}
+    data::Dict{NTuple{3,Int}, Bool}
 end
 
-Hexgrid() = Hexgrid(Dict{CartesianIndex, Bool}())
-Base.getindex(H::Hexgrid, I::Int...) = getindex(H, CartesianIndex(I))
-Base.getindex(H::Hexgrid, I::NTuple{N,Int}) where {N} = getindex(H, CartesianIndex(I))
-Base.getindex(H::Hexgrid, I) = get(H.data, I, false) # tiles are white by default
-Base.setindex!(H::Hexgrid, v, I::Int...) = setindex!(H, v, CartesianIndex(I))
-Base.setindex!(H::Hexgrid, v, I) = (H.data[I] = v)
+Hexgrid() = Hexgrid(Dict{NTuple{3,Int}, Bool}())
+
+# Convenience methods, so that I can index into a Hexgrid like so: grid[1,1,1].
+Base.getindex(H::Hexgrid, I::Int...) = getindex(H, Tuple(I))
+Base.getindex(H::Hexgrid, I::Tuple) = getindex(H, NTuple{3,Int}(I))
+
+# tiles are white by default: 
+Base.getindex(H::Hexgrid, I::NTuple{3,Int}) = get(H.data, I, false)
+# (basically the only reason to use a custom struct instead of the dictionary
+# directly). Means I don't have to worry about out of bounds indexing or
+# extending the grid at the edges (I should've done this for Day 17 as well). 
+
+Base.setindex!(H::Hexgrid, v, I::Int...) = setindex!(H, v, Tuple(I))
+Base.setindex!(H::Hexgrid, v, I::Tuple) = setindex!(H, v, NTuple{3,Int}(I))
+Base.setindex!(H::Hexgrid, v, I::NTuple{3,Int}) = setindex!(H.data, v, I)
+
 Base.copy(H::Hexgrid) = Hexgrid(copy(H.data))
 
 const dirs = Dict(
-        "e"  => CartesianIndex(+1, -1, 0), 
-        "se" => CartesianIndex(0, -1, +1),
-        "sw" => CartesianIndex(-1, 0, +1), 
-        "w"  => CartesianIndex(-1, +1, 0),
-        "nw" => CartesianIndex(0, +1, -1), 
-        "ne" => CartesianIndex(+1, 0, -1)
+        "e"  => (+1, -1, 0), 
+        "se" => (0, -1, +1),
+        "sw" => (-1, 0, +1), 
+        "w"  => (-1, +1, 0),
+        "nw" => (0, +1, -1), 
+        "ne" => (+1, 0, -1)
       )
 
 function parse_input(input)
     lines = splitlines(input)
     directions_regex = r"e|se|sw|w|nw|ne"
-    return [
-         mapreduce(x -> dirs[x], +, 
-                   (m.match for m in eachmatch(directions_regex, l))) 
-         for l in lines
-        ]
+    tiles = Vector{NTuple{3,Int}}()
+    for l in lines
+        matches = (m.match for m in eachmatch(directions_regex, l))
+        directions = map(x -> dirs[x], matches)
+        push!(tiles, broadcast(+, directions...))
+    end
+    return tiles
 end
 
 function set_up_lobby(tiles)
@@ -43,45 +55,33 @@ function set_up_lobby(tiles)
 end
 
 function neighbours(tile)
-    K = Vector{CartesianIndex}()
+    K = Vector{NTuple{3,Int}}()
     for d in values(dirs)
-        push!(K, tile + d)
+        push!(K, tile .+ d)
     end
     return K
 end
 
 function step(grid)
     out = copy(grid)
-    living = Vector{CartesianIndex}()
-    for (k, v) in grid.data
-        if v
-            push!(living, k)
+    tiles_to_check = Set{NTuple{3,Int}}()
+    for (coordinates, color) in grid.data
+        if color == true
+            push!(tiles_to_check, coordinates)
+            push!(tiles_to_check, neighbours(coordinates)...)
         end
     end
-    checked = Vector{CartesianIndex}()
-    for I in living
-        c = 0
-        for J in neighbours(I)
-            if grid[J] == true
-                c += 1
-            end
-            J in checked && continue
-            s = 0
-            for K in neighbours(J)
-                if grid[K]
-                    s += 1
-                end
-            end
-            if grid[J] == true && s ∉ 1:2
-                out[J] = false
-            elseif grid[J] == false && s == 2
-                out[J] = true
+    for t_ind in tiles_to_check
+        count = 0
+        for n_ind in neighbours(t_ind)
+            if grid[n_ind] == true
+                count += 1
             end
         end
-        if grid[I] == true && c ∉ 1:2
-            out[I] = false
-        elseif grid[I] == false && c == 2
-            out[I] = true
+        if grid[t_ind] == true && count ∉ 1:2
+            out[t_ind] = false
+        elseif grid[t_ind] == false && count == 2
+            out[t_ind] = true
         end
     end
     return out
@@ -94,7 +94,7 @@ function solve(input = raw_input)
 end
 
 function part1(grid)  
-    # TODO filter out even numbered tiles
+    # TODO filter out even numbered tiles?
     return count(values(grid.data))
 end
 
